972867474f95badd7d7f5c8cc9632b7367b00439
nikta hajighafouri <83319221+niktahajighafouri@users.noreply.github.com>
2025-04-01 19:42:59 +0330

    005:main:refactor projectname

--- GolinguBackend/GollinguCore/wsgi.py
+++ GolinguBackend/GollinguCore/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Digikala project.
+WSGI config for GolinguBackend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Digikala.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GolinguBackend.settings')
 
 application = get_wsgi_application()
